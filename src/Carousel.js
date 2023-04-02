1ccd70c5481b4a204cc11d18873d43cf5b577ac7
jseiglie <javierseiglie@gmail.com>
2023-04-02 15:19:00 +0200

    pic sizing

--- src/Carousel.js
+++ src/Carousel.js
@@ -16,40 +16,40 @@ const Carousel = () => {
         data-bs-ride="carousel"
       >
         <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="3000">
+          <div className="carousel-item text-center active" data-bs-interval="3000">
           <span className="text-white gx-0">
             <Mensajes />
             </span>
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
+          <div className="carousel-item text-center" data-bs-interval="2000">
             <img
               src={one}
-              className="d-block w-100"
-              alt="https://picsum.photos/200"
+              className="d-block carousel_img"
+              alt="Jose y Marta"
             />
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
+          <div className="carousel-item text-center" data-bs-interval="2000">
             <img
               src={two}
-              className="d-block w-100"
-              alt="https://picsum.photos/200"
+              className="d-block carousel_img "
+              alt="Jose y Marta"
             />
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
+          <div className="carousel-item text-center" data-bs-interval="2000">
             <img
               src={three}
-              className="d-block w-100"
-              alt="https://picsum.photos/200"
+              className="d-block carousel_img"
+              alt="Jose y Marta"
             />
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
+          <div className="carousel-item text-center" data-bs-interval="2000">
             <img
               src={four}
-              className="d-block w-100"
-              alt="https://picsum.photos/200"
+              className="d-block carousel_img"
+              alt="Jose y Marta"
             />
           </div>
-          <div className="carousel-item" data-bs-interval="20000">
+          <div className="carousel-item text-center" data-bs-interval="20000">
             <Gift/>
           </div>
         </div>
